3bb50944e8814b7bd198356f5e9b7e7097b0f817
hamnaiftikhar <hamna.iftikhar123@gmail.com>
2021-12-20 16:32:15 +0500

    first file added

---
+++ first.py
@@ -0,0 +1,44 @@
+def encrypt(text,s):
+    result = ""
+
+    for i in range(len(text)):
+        char = text[i]
+ 
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+ 
+       
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+ 
+    return result
+
+n = input(" ENTER THE TASK YOU WANT TO ENCRYPT  : ")
+
+s = 3
+print ("CURRENT DATA " + n)
+print ("SHIFTED DATA : " + str(s))
+print ("ENCRYPTED DATA : " + encrypt(n,s))
+
+def decrypt(text,s):
+    result = ""
+
+    for i in range(len(text)):
+        char = text[i]
+ 
+        if (char.isupper()):
+            result += chr((ord(char) - s + 65) % 26 + 65)
+ 
+       
+        else:
+            result += chr((ord(char) - s + 97) % 26 + 97)
+ 
+    return result
+
+n = input(" ENTER THE TASK YOU WANT TO DECRYPT : ")
+
+s = 3
+print ("CURRENT" + n)
+print ("SHIFTED DATA : " + str(s))
+print ("DECRYPTED DATA: " + decrypt(n,s))
+    
